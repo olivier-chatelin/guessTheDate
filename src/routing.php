6400fcf6ddefaf195846ed6252408810ad8519d3
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-05-04 14:43:34 +0200

    scoreview ok

--- src/routing.php
+++ src/routing.php
@@ -14,13 +14,8 @@ $routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: HOME_PAGE);
 $controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
 $method = $routeParts[1] ?? '';
 $vars = array_slice($routeParts, 2);
-if ($controller === 'App\Controller\AdminController') {
-    if (empty($_SESSION) || !isset($_SESSION) || !$_SESSION['is_admin']) {
-        header("HTTP/1.0 404 Not Found");
-        echo '404 - Page not found';
-        exit();
-    }
-}
+
+
 if (class_exists($controller) && method_exists(new $controller(), $method)) {
     echo (new $controller())->$method(...$vars);
 } else {
