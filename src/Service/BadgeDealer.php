c36987fde46856c8fe0d12316bb73ba7cc5658e5
Olivier Joubert <olivier.joubert@gmail.com>
2021-05-06 22:09:34 +0200

    Cleaning Game checker and Badge dealer

--- src/Service/BadgeDealer.php
+++ src/Service/BadgeDealer.php
@@ -6,7 +6,13 @@ use App\Model\BadgeManager;
 
 class BadgeDealer
 {
-    public function checkFirstPerfect(int $userId, int $badgeId)
+    public const BADGE_PERFECT = 4;
+    public const BADGE_100K = 8;
+    public const BADGE_FIRSTBESTSCORE = 1;
+    public const BADGE_EASTER = 10;
+    public const BADGE_ALLDEPTS = 3;
+
+    public function checkBadgeAttribution(int $userId, int $badgeId)
     {
         $badgeManager = new BadgeManager();
         $alreadyGotTheBadge = $badgeManager->hasTheBadgeBeenGiven($userId, $badgeId);
@@ -17,5 +23,14 @@ class BadgeDealer
             $badgeInfo = $badgeManager->getInfoBadgeToGive($userId, $badgeId);
         }
         return $badgeInfo;
+    }
+
+    public function checkBadgePerfect()
+    {
+        $shouldReceivedBadge = 0;
+        if ($_SESSION['game']['status'] === 'Perfect') {
+            $shouldReceivedBadge = $this->checkBadgeAttribution($_SESSION['id'], self::BADGE_PERFECT);
+        }
+        return $shouldReceivedBadge;
     }
 }
